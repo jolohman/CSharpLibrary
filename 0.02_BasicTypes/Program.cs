2fc1af19a1f6df52ea28dc97e3a51df31acf37bc
Josh Lohman <jjosh9708@gmail.com>
2017-10-25 11:15:01 -0400

    CSharp Intro Modules and Intial commit

---
+++ 0.02_BasicTypes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._02_BasicTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte myByte = 255; // range: 0 - 255
+
+            int number1 = 15;
+
+            float myFloat = 3.14f;
+
+            double myDouble = 2.54;
+
+            decimal myDecimal = 3.14159M;
+
+            char myLetter = 'A';
+
+            string myName = "Jay"; //up to 2Gigs of characters
+
+            bool myTruth = true;
+
+
+            //Declaration
+            //<type> <variableName> = <optional initializer>
+            int age;
+            string country;
+
+            //Initialization 
+            age = 41; //It is an interger so can't be written "41", has to be 41
+            Console.WriteLine(age);
+            age = 28;
+            Console.WriteLine(age);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 0.02_BasicTypesChallenges/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._02_BasicTypesChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 28;
+
+            string myName = "Samuel";
+
+            bool nonTruth = false;
+
+            float birthYear = 1989;
+
+            double currentYear = 2017;
+
+            decimal exactAge = 28.45M;
+
+            //Declaration with Intializer
+
+            int yearsOld;
+
+            string countryBorn;
+
+            bool theTruth;
+
+            float birthDay;
+
+            double month;
+
+            decimal upcomingAge;
+
+            yearsOld = 28;
+
+            birthDay = 3;
+
+            month = 6;
+
+            countryBorn = "I was born in USA and I'm currently " + yearsOld + " years old born on " + birthYear + "/" + month + "/" + birthDay;
+            Console.WriteLine(countryBorn);
+
+            Console.ReadLine();
+
+        }
+    }
+}
