78cccd6a5101384b4f8353d9e70b0ec92d6223f2
Josh Lohman <jjosh9708@gmail.com>
2017-10-26 15:53:17 -0400

    Dictionary Bronze, Silver, Gold

--- 0.09_CollectionDictionaries/Program.cs
+++ 0.09_CollectionDictionaries/Program.cs
@@ -11,15 +11,15 @@ namespace _0._09_CollectionDictionaries
         static void Main(string[] args)
         {
             //Bronze
-            Dictionary<int, string> openWith = new Dictionary<int, string>();
+            Dictionary<string, string> awardWinners = new Dictionary<string, string>();
 
-            openWith.Add(2000, "American Beauty");
-            openWith.Add(2001, "Gladiator");
-            openWith.Add(2002, "A Beautiful Mind");
-            openWith.Add(2003, "Chicago");
-            openWith.Add(2005, "The Lord of the Rings: The Return of the King");
+            awardWinners.Add("2006", "Happy Feet");
+            awardWinners.Add("2007", "Ratatouille");
+            awardWinners.Add("2008", "Wall-E");
+            awardWinners.Add("2009", "UP");
+            awardWinners.Add("2010", "Toy Story 3");
 
-            foreach (var movie in openWith)
+            foreach (var movie in awardWinners)
             {
                 Console.WriteLine($"{movie.Key} the winner was {movie.Value}");
             }
@@ -41,6 +41,41 @@ namespace _0._09_CollectionDictionaries
                 Console.WriteLine($"Nominees for {nominee.Key}: {nominee.Value[0]}, {nominee.Value[1]}, {nominee.Value[2]}");
             }
 
+            //Gold
+            bool run = true;
+            while (run)
+            {
+                Console.WriteLine("Enter a year");
+                string inputYear = Console.ReadLine();
+
+                if (awardWinners.ContainsKey(inputYear) && awardNominees.ContainsKey(inputYear))
+                {
+                    Console.WriteLine("To show winner, enter 'winner'. \n" +
+                        "To show Nominees, enter 'nominees'. \n" +
+                        "To end the application, enter 'stop.");
+                    string operation = Console.ReadLine().ToLower();
+                    switch (operation)
+                    {
+                        case "winner":
+                            Console.WriteLine($"The winner of the {inputYear} was {awardWinners[inputYear]}. \n\n");
+                            break;
+                        case "nominees":
+                            string nominees = String.Join(",", awardNominees[inputYear]);
+                            Console.WriteLine($"The nominees of the {inputYear} were; {nominees}. \n\n");
+                            break;
+                        case "stop":
+                            run = false;
+                            break;
+                        default:
+                            Console.WriteLine("Sorry, thats not a valid input.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a year between 2006 and 2010. \n\n");
+                }
+            }
 
             Console.ReadLine();
 
