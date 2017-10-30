109038f6b6c07ddffdc722ec3d628c1b237936cb
Josh Lohman <jjosh9708@gmail.com>
2017-10-30 16:50:18 -0400

    Intial commit

---
+++ 0.11_OOP_Game/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._11_OOP_Game
+{
+    class Player: Character
+    {
+        //Enum
+        public enum Specialization
+        {
+            HorseMange = 1,
+            TrollCat = 2,
+            KnightTemlatetor = 3,
+            Demogorg = 4,
+            SireCena = 5
+        }
+    }
+}
---
+++ 0.11_OOP_Game/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._11_OOP_Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Character myName = new Character();
+            Player newHero = new Player();
+            Enemy jenn = new Enemy();
+
+            Console.WriteLine("Hello, wanderer..\n" +
+                "What be thy name?");
+            newHero.Name = Console.ReadLine();
+
+            Console.Clear();
+
+            Console.WriteLine($"Nice to meet you, {newHero.Name}.");
+
+            Console.WriteLine("What is you specialization friend? \n" +
+                "1:Horse Mange\n" +
+                "2:Troll Cat \n" +
+                "3:Knight Templatetor\n" +
+                "4:Demogorg \n" +
+                "5:Sire Cena");
+
+            Player.Specialization = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+
+            Console.ReadLine();
+        }
+    }
+}
