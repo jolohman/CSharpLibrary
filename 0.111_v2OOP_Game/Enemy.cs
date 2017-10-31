e6700d2e98a3d1ede68fbfc04ffe77a8c192aba3
Josh Lohman <jjosh9708@gmail.com>
2017-10-31 11:59:55 -0400

    New Game Console

---
+++ 0.111_v2OOP_Game/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._111_v2OOP_Game
+{
+    class Enemy : Character
+    {
+        //Constructor should have the same name as the class
+        public Enemy(string name, int level)
+        {
+            this.Name = name;
+            this.IsAlive = true;
+            this.Health = 100;
+            this.Level = level;
+            this.AttackPower = 8;
+            this.CritChance = 0.20;
+        }
+
+        //Properties
+        public string Name { get; set; }
+    }
+}
